becb4592d7ad866ec7ff090dfbb5ced7e22422be
haya <hs2439363@gmail.com>
2025-11-30 22:54:55 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+
+const reveals = document.querySelectorAll(".reveal");
+  
+    function revealOnScroll() {
+      for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const revealTop = reveals[i].getBoundingClientRect().top;
+        const revealPoint = 150;
+  
+        if (revealTop < windowHeight - revealPoint) {
+          reveals[i].classList.add("active");
+        } else {
+          reveals[i].classList.remove("active");
+        }
+      }
+    }
+  
+    window.addEventListener("scroll", revealOnScroll);
+    window.addEventListener("load", revealOnScroll);
+    
